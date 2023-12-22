957816398d14cccbae8b8efcf6c2496829ce436d
Henrik Matthiesen <henrikmatthiesen11@gmail.com>
2023-12-22 17:50:11 -0600

    acommodated for changes in network architecture

--- vanilla_network/backpropagation.hpp
+++ vanilla_network/backpropagation.hpp
@@ -17,6 +17,7 @@
 
 
 //forward propagation update in layer at depth i
+//except output layer
 template<size_t N>
 void neuralNetwork<N>::updateLayer_(const size_t i){
     assert(i>0);
@@ -34,12 +35,22 @@ void neuralNetwork<N>::updateLayer_(const size_t i){
             updatedEntry+=(weight*activationFct_(neuronStates_[i-1].getEntry(prevCoord)));
         }
         updatedEntry+=biases_[i].getEntry(currCoord);
-        //updatedEntry=this->activationFct_(updatedEntry);
-        //new version stores values of neurons before applying activation to it
+        //this version stores values of neurons before applying activation to it
         neuronStates_[i].getEntry(currCoord)=updatedEntry;
     }
     return;
 }
+
+
+
+template<size_t N>
+void neuralNetwork<N>::updateOutputLayer_(void){
+    transformInPlace(neuronStates_[neuronStates_.size()-2],neuronStates_[neuronStates_.size()-2],activationFct_);
+    neuronStates_[neuronStates_.size()-1]=outputLayerTransform_(neuronStates_[neuronStates_.size()-2]);
+    return;
+}
+
+
 
 //forward propagation through entire network
 template<size_t N>
@@ -49,11 +60,14 @@ void neuralNetwork<N>::updateNeuronStates_(const Tensor<double,N-1>& input){
         throw(std::domain_error("neuralNetwork<N>::updateNeuronStates_: input shape does not match network shape"));
     }
     neuronStates_[0]=input;
-    for(size_t i=1; i<neuronStates_.size(); i++){
+    for(size_t i=1; i<neuronStates_.size()-1; i++){
         updateLayer_(i);
     }
+    updateOutputLayer_();
     return;
 }
+
+
 
 //predict
 template<size_t N>
@@ -64,12 +78,11 @@ Tensor<double,N-1> neuralNetwork<N>::predict(
     if(updateNeuronStates){
         updateNeuronStates_(input);
     }
-    Tensor<double,N-1> pred(neuronStates_.shape.shape.back());
-    for(const auto& c : coordinates_.back()){
-        pred.getEntry(c)=activationFct_(neuronStates_[neuronStates_.size()-1].getEntry(c));
-    }
-    return pred;
+    return neuronStates_[neuronStates_.size()-1];
 }
+
+
+
 
 
 //backpropagation all, but output layer
@@ -91,18 +104,29 @@ void neuralNetwork<N>::updateGradient_(
     return;
 }
 
+
 template<size_t N>
 void neuralNetwork<N>::updateOutputLayerGrad_(
     const Tensor<double, N-1>& sampleIn,
     const Tensor<double,N-1>& sampleOut
 ){
     //call from updateGradient_ which updates neuronStates before
+
     Tensor<double,N-1> pred = predict(sampleIn, false);
     for(const auto& c : coordinates_[neuronStates_.size()-1]){
-        neuronGrad_[neuronStates_.size()-1].getEntry(c)=
-            costFctGrad_(pred, sampleOut, c, neuronCount_)
-            *activationFctGrad_(neuronStates_[neuronStates_.size()-1].getEntry(c))
-        ;
+        neuronGrad_[neuronStates_.size()-1].getEntry(c) = costFctGrad_(pred, sampleOut, c, outputLayerCount_);
+    }
+
+    Tensor<double,2*N-2> grad = outputLayerTransformGrad_(neuronStates_[neuronStates_.size()-2]);
+    for(const auto& c : coordinates_[neuronStates_.size()-1]){
+        neuronGrad_[neuronStates_.size()-2].getEntry(c)=0;
+        for(const auto& d: coordinates_[neuronStates_.size()-1]){
+            std::vector<size_t> gradPos=c;
+            gradPos.insert(gradPos.end(),d.begin(),d.end());
+            neuronGrad_[neuronStates_.size()-2].getEntry(c)
+                +=neuronGrad_[neuronStates_.size()-1].getEntry(d)
+                *grad.getEntry(gradPos);
+        }
     }
     return;
 }
@@ -110,10 +134,12 @@ void neuralNetwork<N>::updateOutputLayerGrad_(
 //compute gradient in neuron state variables for all hidden and input layers
 template<size_t N>
 void neuralNetwork<N>::backpropagation_()
-{   
-   
-    for(size_t layer = coordinates_.size()-2; layer<coordinates_.size()-1; layer--){
+{    
+   //backpropagate neuron gradients
+    for(size_t layer = coordinates_.size()-3; layer<coordinates_.size()-1; layer--){
+        //all but output layer
         for(const auto& c : coordinates_[layer]){
+            //reset to zero, because old information may be stored there
             neuronGrad_[layer].getEntry(c)=0;
             for(const auto& d : coordinates_[layer+1]){
                 std::vector<size_t> weightCoord = {layer};
@@ -133,7 +159,7 @@ void neuralNetwork<N>::backpropagation_()
 //updates all weights assuming that neuronGrad_ has been updated
 template<size_t N>
 void neuralNetwork<N>::updateWeightsGrad_(size_t batchIdx){
-    for(size_t layer=0; layer<neuronStates_.size()-1; layer++){
+    for(size_t layer=0; layer<neuronStates_.size()-2; layer++){
         for(const auto& outCoord : coordinates_[layer]){
             //update weight going out of outCoord
             double updatedEntry=0;
@@ -147,11 +173,9 @@ void neuralNetwork<N>::updateWeightsGrad_(size_t batchIdx){
                 if(batchIdx==0){
                     weightsGrad_.getEntry(weightCoord)=0;
                 }
-//now correct, no!
                 weightsGrad_.getEntry(weightCoord)+=
                     neuronGrad_[layer+1].getEntry(inCoord)
                     *activationFct_(neuronStates_[layer].getEntry(outCoord));
-                    //*weights_.getEntry(weightCoord)
                 ;
             }
         }
@@ -165,7 +189,7 @@ void neuralNetwork<N>::updateWeightsGrad_(size_t batchIdx){
 //updates all biases, assume neuronGrad_ has been updated
 template<size_t N>
 void neuralNetwork<N>::updateBiasesGrad_(size_t batchIdx){
-    for(size_t layer=1; layer<neuronStates_.size(); layer++){
+    for(size_t layer=1; layer<neuronStates_.size()-1; layer++){
         for(const auto& c : coordinates_[layer]){
             if(batchIdx==0){
                 biasesGrad_[layer].getEntry(c)=0;
